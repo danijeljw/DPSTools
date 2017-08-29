64329b150e953a83d66aef5895fa433092704322
Danijel-James Wynyard <danijel.james@protonmail.ch>
2017-08-29 15:32:55 +0930

    Create basic_menu.ps1

---
+++ LICENSE.md/basic_menu.ps1
@@ -0,0 +1,38 @@
+# Source - http://www.tomsitpro.com/articles/powershell-interactive-menu,2-961.html
+
+function Show-Menu
+{
+     param (
+           [string]$Title = 'My Menu'
+     )
+     Clear-host
+     Write-Host "================ $Title ================"
+    
+     Write-Host "1: Press '1' for this option."
+     Write-Host "2: Press '2' for this option."
+     Write-Host "3: Press '3' for this option."
+     Write-Host "Q: Press 'Q' to quit."
+}
+
+do
+{
+     Show-Menu
+     $input = Read-Host "Please make a selection"
+     switch ($input)
+     {
+           '1' {
+                Clear-Host
+                'You chose option #1'
+           } '2' {
+                Clear-Host
+                'You chose option #2'
+           } '3' {
+                Clear-Host
+                'You chose option #3'
+           } 'q' {
+                return
+           }
+     }
+     pause
+}
+until ($input -eq 'q')
